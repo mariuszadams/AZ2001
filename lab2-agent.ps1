b916d62e83a00fb87d728c3d630ea881053c182c
mariuszadams <37147820+mariuszadams@users.noreply.github.com>
2024-05-21 13:12:36 +0200

    lab2 agent

---
+++ lab2-agent.ps1
@@ -0,0 +1,3 @@
+# install the agent
+mkdir agent ; cd agent
+Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$HOME\Downloads\vsts-agent-win-x64-3.232.0.zip", "$PWD")
